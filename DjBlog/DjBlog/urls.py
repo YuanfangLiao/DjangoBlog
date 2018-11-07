a8629503695e9fcd5720a063de2c70b4e24fe748
Yuanfang <305480784@qq.com>
2018-11-07 22:56:24 +0800

    v0.3
    完善了头像修改的用户体验，更改了主页视图，增加分页功能，修改了用户后台布局

--- DjBlog/DjBlog/urls.py
+++ DjBlog/DjBlog/urls.py
@@ -13,19 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
+from django.conf.urls import url
+# from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 import blog.views
+from django.views import static
 from DjBlog import settings
 from blog import urls as blog_url
 from users import urls as users_url
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', blog.views.index),
-                  path('blog/', include((blog_url, 'blog'), namespace='blog')),
-                  path('users/', include((users_url, 'users'), namespace='users')),
+    path('admin/', admin.site.urls),
+    path('', blog.views.index),
+    path('blog/', include((blog_url, 'blog'), namespace='blog')),
+    path('users/', include((users_url, 'users'), namespace='users')),
 
-              ]
+    # 增加以下一行，以识别静态资源
+    url(r'^static/(?P<path>.*)$', static.serve,
+        {'document_root': settings.STATIC_ROOT}, name='static')
+
+]
